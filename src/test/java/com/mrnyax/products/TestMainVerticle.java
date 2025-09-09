2f127e8c1783830d74c97cb8e0995881e24a55b6
nyakoria <nyakoria@gmail.com>
2025-09-09 20:59:16 +0300

    test: check web verticle is deployed

--- src/test/java/com/mrnyax/products/TestMainVerticle.java
+++ src/test/java/com/mrnyax/products/TestMainVerticle.java
@@ -1,22 +1,45 @@
 package com.mrnyax.products;
 
+import com.mrnyax.products.verticles.WebVerticle;
+import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(VertxExtension.class)
 public class TestMainVerticle {
 
-  @BeforeEach
-  void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
-    vertx.deployVerticle(new MainVerticle()).onComplete(testContext.succeeding(id -> testContext.completeNow()));
+  @Test
+  public void mainVerticlesIsDeployed(Vertx vertx, VertxTestContext testContext) {
+    vertx.deployVerticle(new MainVerticle())
+      .onComplete(testContext.succeeding(id -> {
+        testContext.verify(() -> {
+          assertNotNull(id);
+          testContext.completeNow();
+        });
+      }));
   }
 
   @Test
-  void verticle_deployed(Vertx vertx, VertxTestContext testContext) throws Throwable {
-    testContext.completeNow();
+  void mainVerticle_deploysWebVerticle() throws Exception {
+    // Arrange
+    Vertx mockVertx = mock(Vertx.class);
+    when(mockVertx.deployVerticle(anyString()))
+      .thenReturn(Future.succeededFuture("web-deploy-id"));
+
+    MainVerticle main = new MainVerticle();
+    main.init(mockVertx, null);
+
+    // Act
+    main.start();
+
+    // Assert
+    verify(mockVertx, times(1)).deployVerticle(eq(WebVerticle.class.getName()));
+    verifyNoMoreInteractions(mockVertx);
   }
 }
